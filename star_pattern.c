022273b09a82a5e1de989c497bb55a83784f364a
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-04 16:35:47 +0600

    nested loop and pattern

---
+++ star_pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+
+        printf("\n");
+        
+    }
+    
+
+    return 0;
+};
