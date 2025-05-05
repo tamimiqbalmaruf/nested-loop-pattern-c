7094f70fb7584affc131e05ce435f39dd4fdc086
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-05 16:03:56 +0600

    quiz

---
+++ quiz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+    for (int i = 3; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            // printf("%d", num++);
+            printf("*");
+        }
+        printf(" ");
+    }
+
+    return 0;
+};
